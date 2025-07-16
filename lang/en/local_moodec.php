c09e36510f7f69ce1beb363a3c21885d4e5a42e5
Vernon Spain <verzog@gmail.com>
2025-07-16 15:55:27 +1000

    Update local_moodec.php
    chatgpt

--- lang/en/local_moodec.php
+++ lang/en/local_moodec.php
@@ -2,16 +2,31 @@
 /**
  * Moodec Language file
  *
- * @package     local
- * @subpackage  local_moodec
- * @author   	Thomas Threadgold
+ * @package     local_moodec
+ * @author      Thomas Threadgold, OpenAI Updates
  * @copyright   2015 LearningWorks Ltd
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
+defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Moodec';
+
+// General
+$string['moodec'] = 'Moodec';
+$string['privacy:metadata'] = 'The Moodec plugin does not store personal data directly.';
+
+// New settings additions
+$string['currency_format_locale'] = 'Currency locale';
+$string['currency_format_locale_desc'] = 'The locale used to format currency (e.g. en_AU, en_US, fr_FR).';
+$string['currency_symbol'] = 'Currency symbol';
+$string['currency_symbol_desc'] = 'Symbol used for currency display (e.g. $, €, ¥).';
+
+// New UI messages
+$string['tooltip_add_to_cart'] = 'Click to add this course to your cart';
+$string['tooltip_checkout'] = 'Proceed to payment options';
+$string['tooltip_enrolled'] = 'You are already enrolled in this course';
+$string['tooltip_view_details'] = 'Click to view product details';
 
 // SETTINGS
 $string['moodec_pages'] = 'Page settings';
@@ -49,12 +64,16 @@ $string['page_product_show_category_desc'] = 'This will show the course category
 $string['page_product_show_related_products'] = 'Show related products';
 $string['page_product_show_related_products_desc'] = 'This will show the related products on the product page';
 
-$string['businessemail'] = 'PayPal business email';
-$string['businessemail_desc'] = 'The email address of your business PayPal account';
+// Currency settings for localisation
 $string['currency'] = 'Currency';
+$string['currency_desc'] = 'The 3-character ISO currency code (e.g. USD, AUD, EUR).';
+$string['currency_locale'] = 'Locale';
+$string['currency_locale_desc'] = 'Sets locale-based formatting for currency (e.g. en_AU, fr_FR).';
+
 $string['pagination'] = 'Courses per page';
 $string['pagination_desc'] = 'The number of courses to be displayed per page in the catalogue';
 
+// Payment
 $string['payment_title'] = 'Payment Gateways';
 $string['payment_enable'] = 'Enable';
 $string['payment_enable_desc'] = 'Enable this payment gateway';
@@ -70,133 +89,3 @@ $string['payment_paypal_email'] = 'Email';
 $string['payment_paypal_email_desc'] = 'Enter your Paypal business email address';
 $string['payment_paypal_sandbox'] = 'Use sandbox mode';
 $string['payment_paypal_sandbox_desc'] = 'This will enable Paypal sandbox for the gateway';
-
-// Errors
-$string['error_dpsinitiate'] = 'Could not initiate a transaction with the DPS payment server - please try again later.';
-$string['error_invalid_name'] = 'Required field. Please enter a name.';
-$string['error_invalid_price'] = 'Invalid format: please enter a single number only, such as 20 or 19.99 - no currency symbols or letters are allowed.';
-$string['error_invalid_duration'] = 'Invalid format: please enter a single number, indicating the days for the duration of the course. The number 0 represents an unlimited duration.';
-
-$string['moodec:viewalltransactions'] = 'View all Moodec transactions';
-$string['messageprovider:payment_gateway'] = 'Notifications from the Moodec Payment Gateway';
-
-// Pages
-
-// CATALOGUE
-$string['catalogue_title'] = 'Store';
-$string['catalogue_empty'] = 'No products available.';
-$string['catalogue_enrolment_duration_label'] = 'Course duration:';
-$string['filter_category_label'] = 'Category:';
-$string['filter_sort_label'] = 'Sort by:';
-$string['filter_sort_default_asc'] = 'Default';
-$string['filter_sort_fullname_asc'] = 'Course Title: A - Z';
-$string['filter_sort_fullname_desc'] = 'Course Title: Z - A';
-$string['filter_sort_price_asc'] = 'Price: Low to High';
-$string['filter_sort_price_desc'] = 'Price: High to Low';
-$string['filter_sort_duration_asc'] = 'Duration: Low to High';
-$string['filter_sort_duration_desc'] = 'Duration: High to Low';
-$string['course_list_category_label'] = 'Category:';
-
-// PRODUCT
-$string['product_title'] = '{$a->coursename}';
-$string['enrolment_duration_label'] = 'Course duration:';
-$string['price_label'] = 'Price:';
-$string['product_related_label'] = 'Related Products';
-$string['product_related_button_label'] = 'View details';
-
-// CHECKOUT
-$string['checkout_title'] = 'Checkout';
-$string['checkout_message'] = 'Please review your cart once more before purchasing.';
-$string['checkout_removed_courses_label'] = 'The following courses have been removed from your cart as they are either invalid, or you are already enrolled in them:';
-$string['checkout_total'] = 'Total:';
-$string['checkout_guest_message'] = 'You cannot be logged in as guest to purchase courses! Please log out and create your own account to continue.';
-
-// CART
-$string['cart_title'] = 'Cart';
-$string['cart_total'] = 'Total:';
-$string['cart_empty_message'] = 'Your cart is empty!';
-
-// TRANSACTIONS
-$string['transactions_title'] = 'Transactions';
-$string['transaction_view_label'] = 'View';
-$string['transaction_status_complete'] = 'Complete';
-$string['transaction_status_failed'] = 'Failed';
-$string['transaction_status_pending'] = 'Pending';
-$string['transaction_status_not_submitted'] = 'Not submitted';
-
-$string['transaction_table_empty'] = 'No transactions to display.';
-
-$string['transaction_field_date'] = 'Date';
-$string['transaction_field_id'] = 'Transaction ID';
-$string['transaction_field_user'] = 'User';
-$string['transaction_field_amount'] = 'Amount';
-$string['transaction_field_items'] = '# Items';
-$string['transaction_field_gateway'] = 'Gateway';
-$string['transaction_field_txn'] = 'Txn ID';
-$string['transaction_field_status'] = 'Status';
-$string['transaction_field_actions'] = '';
-$string['transaction_field_actions_course'] = 'Go to course';
-
-$string['transaction_filter_date'] = 'Date range';
-$string['transaction_filter_date_from'] = 'From';
-$string['transaction_filter_date_to'] = 'To';
-$string['transaction_filter_gateway'] = 'Gateway';
-$string['transaction_filter_status'] = 'Status';
-$string['transaction_filter_button_filter'] = 'Filter';
-$string['transaction_filter_button_reset'] = 'Reset';
-
-$string['transaction_view_title'] = 'Transaction #{$a->id}';
-$string['transaction_section_details'] = 'Details';
-$string['transaction_section_items'] = 'Items';
-$string['transaction_section_error'] = 'Error details';
-
-// Buttons
-$string['button_add_label'] = 'Add to cart';
-$string['button_remove_label'] = 'Remove';
-$string['button_checkout_label'] = 'Proceed to checkout';
-$string['button_cart_empty_label'] = 'Empty cart';
-$string['button_paypal_label'] = 'Pay with PayPal';
-$string['button_dps_label'] = 'Pay via DPS';
-$string['button_return_store_label'] = 'Return to store';
-$string['button_logout_label'] = 'Logout';
-$string['button_enrolled_label'] = 'Already enrolled';
-$string['button_in_cart_label'] = 'In cart';
-
-// Lib
-$string['enrolment_duration_unlimited'] = 'Unlimited';
-$string['enrolment_duration_year'] = 'year';
-$string['enrolment_duration_year_plural'] = 'years';
-$string['enrolment_duration_month'] = 'month';
-$string['enrolment_duration_month_plural'] = 'months';
-$string['enrolment_duration_week'] = 'week';
-$string['enrolment_duration_week_plural'] = 'weeks';
-$string['enrolment_duration_day'] = 'day';
-$string['enrolment_duration_day_plural'] = 'days';
-
-// Edit Product form
-$string['edit_product_form_title'] = 'Product settings for {$a->name}';
-$string['product_enabled'] = 'Enable';
-$string['product_enabled_label'] = 'Toggle whether this product is shown in the store and able to be bought';
-$string['product_description'] = 'Description';
-$string['product_description_help'] = 'The Moodec product description. This can be combined with the default Course summary and used as additional information, or as the only means to display information on the product page.';
-$string['product_tags'] = 'Product tags';
-$string['product_tags_help'] = 'List the keyword tags, comma separated, which will be used when performing a search.';
-$string['product_type'] = 'Product type';
-$string['product_type_help'] = 'Select the type of product this is. Simple products have a single price, duration and group that can be assigned to it. Variable products can have up to 10 variations specified - each with its own name, price and duration.';
-$string['product_type_simple_label'] = 'Simple';
-$string['product_type_variable_label'] = 'Variable';
-$string['product_variation_header'] = 'Variation {$a->count}';
-$string['product_variation_enabled'] = 'Enable';
-$string['product_variation_enabled_label'] = 'Toggle whether this variation is active or not';
-$string['product_variation_count'] = 'Number of variations';
-$string['product_variation_count_help'] = 'Select the number of variations for variable product types, up to 10. Simple product types only have 1 variation.';
-$string['product_variation_name'] = 'Name';
-$string['product_variation_name_help'] = 'The name to be displayed in the store for this variation. This is not shown for Simple product types.';
-$string['product_variation_price'] = 'Price';
-$string['product_variation_price_help'] = 'The price, set to 2 decimal places. Do not include currency symbol.';
-$string['product_variation_duration'] = 'Duration';
-$string['product_variation_duration_help'] = 'Enter the number of days for the course enrolment duration. A value of 0 will set an unlimited enrolment duration.';
-$string['product_variation_group'] = 'Group';
-$string['product_variation_group_help'] = 'Select a course group for the user to be assigned when they purchase this variation.';
-$string['product_variations_update'] = 'Update variations form';
-$string['product_variation_group_none'] = 'No group';
