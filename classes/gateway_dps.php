7589ac7cb7b78718985fc31841f10a009d6b3e8b
Vernon Spain <verzog@gmail.com>
2025-07-16 14:56:35 +1000

    Update gateway_dps.php
    chatgpt

--- classes/gateway_dps.php
+++ classes/gateway_dps.php
@@ -1,230 +1,183 @@
-<?php 
+<?php
 /**
  * Moodec Gateway DPS
  *
  * @package     local
  * @subpackage  local_moodec
- * @author   	Thomas Threadgold
- * @copyright   2015 LearningWorks Ltd
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Vernon Spain - Formerly Thomas Threadgold
+ * @copyright  2015 LearningWorks Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Load Moodle config
-require_once dirname(__FILE__) . '/../../../config.php';
-// Load Moodec lib
-require_once dirname(__FILE__) . '/../lib.php';
+namespace local_moodec;
 
-class MoodecGatewayDPS extends MoodecGateway {
+use DomDocument;
+use moodle_url;
+use core\message\message;
 
-	protected $_internalGatewayURL = '';
+require_once(__DIR__ . '/../../../config.php');
+require_once(__DIR__ . '/../lib.php');
+require_once($CFG->libdir . '/filelib.php');
 
-	function __construct($transaction) {
-		global $CFG;
+class gateway_dps extends gateway {
 
-		parent::__construct($transaction);
+    protected $_internalGatewayURL;
 
-		$this->_gatewayName = get_string('payment_dps_title', 'local_moodec');
+    public function __construct($transaction) {
+        global $CFG;
 
-		$this->_internalGatewayURL = new moodle_url($CFG->wwwroot . '/local/moodec/payment/dps/index.php');
+        parent::__construct($transaction);
 
-		// Checks if sandbox mode is enabled
-		if( !!get_config('local_moodec', 'payment_dps_sandbox') ) {
-			$this->_gatewayURL = 'https://uat.paymentexpress.com/pxaccess/pxpay.aspx'; // the DPS sandbox URL
-		} else {
-			$this->_gatewayURL = 'https://sec.paymentexpress.com/pxaccess/pxpay.aspx';
-		}
-	}
+        $this->_gatewayName = get_string('payment_dps_title', 'local_moodec');
+        $this->_internalGatewayURL = new moodle_url($CFG->wwwroot . '/local/moodec/payment/dps/index.php');
 
-	/**
-	 * Turn an XML string into a DOM object.
-	 *
-	 * @param string $xml An XML string
-	 * @return object The SimpleXMLElement object representing the root element.
-	 */
-	public function get_dom($xml) {
-	    $dom = new DomDocument();
-	    $dom->preserveWhiteSpace = false;
-	    $dom->loadXML($xml);
-	    return simplexml_import_dom($dom);
-	}
+        $this->_gatewayURL = get_config('local_moodec', 'payment_dps_sandbox')
+            ? 'https://uat.paymentexpress.com/pxaccess/pxpay.aspx'
+            : 'https://sec.paymentexpress.com/pxaccess/pxpay.aspx';
+    }
 
-	public function query($data){
-		global $CFG;
+    public function get_dom(string $xml): \SimpleXMLElement {
+        $dom = new DomDocument();
+        $dom->preserveWhiteSpace = false;
+        $dom->loadXML($xml);
+        return simplexml_import_dom($dom);
+    }
 
-		require_once $CFG->libdir . '/filelib.php';
+    public function query(string $data): string {
+        $curl = new \curl();
+        return $curl->post($this->_gatewayURL, $data, ['returntransfer' => true]);
+    }
 
-		$c = new curl();
-		$options = array(
-			'returntransfer' => true
-		);
+    public function begin(): \SimpleXMLElement {
+        global $CFG, $USER;
 
-		$result = $c->post($this->_gatewayURL, $data, $options);
+        $txnId = time() . $this->_transaction->get_id();
+        $site = get_site();
 
-		return $result;
-	}
-
-	public function begin(){
-		global $CFG, $USER;
-
-		$txnId = time() . $this->_transaction->get_id();
-		$site = get_site();
-
-        // create the "Generate Request" XML message
         $xmlrequest = sprintf(
-        	"<GenerateRequest>
-            	<PxPayUserId>%s</PxPayUserId>
-            	<PxPayKey>%s</PxPayKey>
-            	<AmountInput>%.2f</AmountInput>
-            	<CurrencyInput>%s</CurrencyInput>
-            	<MerchantReference>%s</MerchantReference>
-            	<EmailAddress>%s</EmailAddress>
-            	<TxnData1>%d</TxnData1>
-            	<TxnData2>%s</TxnData2>
-            	<TxnData3>%s</TxnData3>
-            	<TxnType>Purchase</TxnType>
-            	<TxnId>%d</TxnId>
-            	<BillingId></BillingId>
-            	<EnableAddBillCard>0</EnableAddBillCard>
-            	<UrlSuccess>%s</UrlSuccess>
-            	<UrlFail>%s</UrlFail>
-            	<Opt></Opt>
+            "<GenerateRequest>
+                <PxPayUserId>%s</PxPayUserId>
+                <PxPayKey>%s</PxPayKey>
+                <AmountInput>%.2f</AmountInput>
+                <CurrencyInput>%s</CurrencyInput>
+                <MerchantReference>%s</MerchantReference>
+                <EmailAddress>%s</EmailAddress>
+                <TxnData1>%d</TxnData1>
+                <TxnData2>%s</TxnData2>
+                <TxnData3>%s</TxnData3>
+                <TxnType>Purchase</TxnType>
+                <TxnId>%d</TxnId>
+                <BillingId></BillingId>
+                <EnableAddBillCard>0</EnableAddBillCard>
+                <UrlSuccess>%s</UrlSuccess>
+                <UrlFail>%s</UrlFail>
+                <Opt></Opt>
             </GenerateRequest>",
-            clean_param(get_config('local_moodec', 'payment_dps_userid'), PARAM_CLEAN), // PxPay User ID
-            clean_param(get_config('local_moodec', 'payment_dps_key'), PARAM_CLEAN),	// PxPay Key
-            clean_param($this->_transaction->get_cost(), PARAM_CLEAN), // Amount
-            clean_param(get_config('local_moodec', 'currency'), PARAM_CLEAN),	// Currency
-            clean_param('Transaction #' . $this->_transaction->get_id(), PARAM_CLEAN), // Merchant reference
-            clean_param($USER->email, PARAM_CLEAN), // Email
+            clean_param(get_config('local_moodec', 'payment_dps_userid'), PARAM_CLEAN),
+            clean_param(get_config('local_moodec', 'payment_dps_key'), PARAM_CLEAN),
+            clean_param($this->_transaction->get_cost(), PARAM_CLEAN),
+            clean_param(get_config('local_moodec', 'currency'), PARAM_CLEAN),
+            clean_param('Transaction #' . $this->_transaction->get_id(), PARAM_CLEAN),
+            clean_param($USER->email, PARAM_CLEAN),
             clean_param($txnId, PARAM_CLEAN),
             clean_param(substr($site->shortname, 0, 50), PARAM_CLEAN),
             clean_param(substr("{$USER->lastname}, {$USER->firstname}", 0, 50), PARAM_CLEAN),
-            clean_param(time().$this->_transaction->get_id(), PARAM_CLEAN), // TxnId
-            new moodle_url($CFG->wwwroot . '/local/moodec/payment/dps/success.php'), // URL Success
-            new moodle_url($CFG->wwwroot . '/local/moodec/payment/dps/fail.php') 	// URL Fail
+            clean_param($txnId, PARAM_CLEAN),
+            new moodle_url($CFG->wwwroot . '/local/moodec/payment/dps/success.php'),
+            new moodle_url($CFG->wwwroot . '/local/moodec/payment/dps/fail.php')
         );
 
-		// Query DPS with the xml request
-        $result = $this->query($xmlrequest);
-
-        // Set the transaction gateway and status
         $this->_transaction->set_gateway(MOODEC_GATEWAY_DPS);
         $this->_transaction->set_txn_id($txnId);
         $this->_transaction->pending();
 
-        // Return the DOM formatted result of the request
-        return $this->get_dom($result);
-	}
+        return $this->get_dom($this->query($xmlrequest));
+    }
 
-	public function abort($data){
+    public function abort($data): void {
+        $xmlrequest = sprintf(
+            "<ProcessResponse>
+                <PxPayUserId>%s</PxPayUserId>
+                <PxPayKey>%s</PxPayKey>
+                <Response>%s</Response>
+            </ProcessResponse>",
+            clean_param(get_config('local_moodec', 'payment_dps_userid'), PARAM_CLEAN),
+            clean_param(get_config('local_moodec', 'payment_dps_key'), PARAM_CLEAN),
+            $data
+        );
 
-		// Check transaction response and confirm
-		$xmlrequest = sprintf(
-			"<ProcessResponse>
-		    	<PxPayUserId>%s</PxPayUserId>
-		    	<PxPayKey>%s</PxPayKey>
-		    	<Response>%s</Response>
-		    </ProcessResponse>",
-		    clean_param(get_config('local_moodec', 'payment_dps_userid'), PARAM_CLEAN), // PxPay User ID
-            clean_param(get_config('local_moodec', 'payment_dps_key'), PARAM_CLEAN),	// PxPay Key
-		    $data // DPS result data
-		);
-		$xmlreply = $this->query($xmlrequest);
-		$response = $this->get_dom($xmlreply);
+        $response = $this->get_dom($this->query($xmlrequest));
+        $this->send_error_to_admin("DPS transaction failed!", (array)$response);
+        $this->_transaction->fail();
+    }
 
-		$this->send_error_to_admin("DPS transaction failed!", $response);
+    public function handle($data = null): bool {
+        if ($this->_transaction->get_status() === \MoodecTransaction::STATUS_COMPLETE) {
+            return true;
+        }
 
-		$this->_transaction->fail();
-	}
+        if (is_null($data)) {
+            $this->_transaction->fail();
+            return false;
+        }
 
-	// handle the response	
-	public function handle($data = null){
+        $xmlrequest = sprintf(
+            "<ProcessResponse>
+                <PxPayUserId>%s</PxPayUserId>
+                <PxPayKey>%s</PxPayKey>
+                <Response>%s</Response>
+            </ProcessResponse>",
+            clean_param(get_config('local_moodec', 'payment_dps_userid'), PARAM_CLEAN),
+            clean_param(get_config('local_moodec', 'payment_dps_key'), PARAM_CLEAN),
+            $data
+        );
 
-		// Abort if the transaction is already complete
-		if( $this->_transaction->get_status() === MoodecTransaction::STATUS_COMPLETE ) {
-			return true;
-		}
+        $response = $this->get_dom($this->query($xmlrequest));
 
-		// Check to see if the data is null
-		if( is_null($data) ) {
-			$this->_transaction->fail();
-			return false;
-		}
+        if ($response === false || $response->attributes()->valid != '1') {
+            $this->send_error_to_admin("DPS transaction was not valid!", (array)$response);
+            $this->_transaction->fail();
+            return false;
+        }
 
-		// Check transaction response and confirm
-		$xmlrequest = sprintf(
-			"<ProcessResponse>
-		    	<PxPayUserId>%s</PxPayUserId>
-		    	<PxPayKey>%s</PxPayKey>
-		    	<Response>%s</Response>
-		    </ProcessResponse>",
-		    clean_param(get_config('local_moodec', 'payment_dps_userid'), PARAM_CLEAN), // PxPay User ID
-            clean_param(get_config('local_moodec', 'payment_dps_key'), PARAM_CLEAN),	// PxPay Key
-		    $data // DPS result data
-		);
-		$xmlreply = $this->query($xmlrequest);
-		$response = $this->get_dom($xmlreply);
+        if ($this->_transaction->get_txn_id() != $response->TxnId) {
+            $this->send_error_to_admin("Transaction IDs do not match!", (array)$response);
+            $this->_transaction->fail();
+            return false;
+        }
 
-		// abort if invalid
-		if ($response === false or $response->attributes()->valid != '1') {
-		    $this->send_error_to_admin("DPS transaction was not valid!", $response);
-		    $this->_transaction->fail();
-		    return false;
-		}
+        if ($response->CurrencySettlement != get_config('local_moodec', 'currency')) {
+            $this->send_error_to_admin("Currency mismatch: received {$response->CurrencySettlement}", (array)$response);
+            $this->_transaction->fail();
+            return false;
+        }
 
-		// Check that the transaction id matches the response one
-		if ($this->_transaction->get_txn_id() != $response->TxnId) {
-		    $this->send_error_to_admin("Transaction IDs do not match! This ID: " . $this->_transaction->get_id() . ", Response TxnId: " . $response->TxnId, $response);
-		    $this->_transaction->fail();
-		    return false;
-		}
+        if ($response->AmountSettlement < $this->_transaction->get_cost()) {
+            $this->send_error_to_admin("Amount paid too low: {$response->AmountSettlement} < {$this->_transaction->get_cost()}", (array)$response);
+            $this->_transaction->fail();
+            return false;
+        }
 
-		// Confirm currency is correctly set and matches the plugin config
-		if ($response->CurrencySettlement != get_config('local_moodec', 'currency')) {
-			$this->send_error_to_admin("Currency does not match course settings, received: " . $response->CurrencySettlement, $response);
-			$this->_transaction->fail();
-			return false;
-		}
+        if ($response->Success == 1 && $response->ResponseText == "APPROVED") {
+            if ($this->verify_transaction()) {
+                $this->complete_enrolment();
+                return true;
+            }
+        }
 
-		// Check if the payment was less than the transaction cost
-		if( $response->AmountSettlement < $this->_transaction->get_cost() ) {
-			$this->send_error_to_admin("Amount paid is not enough (".$response->AmountSettlement." < ".$this->_transaction->get_cost().")", $response);
-			$this->_transaction->fail();
-			return false;
-		}
+        $this->send_error_to_admin("Unhandled error completing DPS transaction", (array)$response);
+        return false;
+    }
 
-		// enrol and continue if DPS returns "APPROVED"
-		if ($response->Success == 1 and $response->ResponseText == "APPROVED") {
-			// Lastly, verify the general transaction items and user
-			if( $this->verify_transaction() ) {
-				
-				$this->complete_enrolment();
-
-				return true;
-			}
-
-		}
-
-		$this->send_error_to_admin("Something uncaught prevented the transaction from completing!", $response);
-		return false;
-	}
-
-	public function render(){
-
-		// output form
-		$html = sprintf('<form action="%s" method="POST" class="payment-gateway gateway--dps">', $this->_internalGatewayURL);
-
-			$html .= sprintf(
-				'<input type="hidden" name="id" value="%s">',
-				$this->_transaction->get_id()
-			); 
-
-		 	$html .= sprintf(
-		 		'<input type="submit" name="submit"  value="%s">',
-		 		get_string('button_dps_label', 'local_moodec')
-		 	);
-
-		$html .= sprintf('</form>');
-
-		return $html;
-	}
+    public function render(): string {
+        return sprintf(
+            '<form action="%s" method="POST" class="payment-gateway gateway--dps">
+                <input type="hidden" name="id" value="%s">
+                <input type="submit" name="submit" value="%s">
+            </form>',
+            $this->_internalGatewayURL,
+            $this->_transaction->get_id(),
+            get_string('button_dps_label', 'local_moodec')
+        );
+    }
 }
