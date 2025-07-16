473f2904d2c941e3a09a574109dfd8fcc4993f7c
Vernon Spain <verzog@gmail.com>
2025-07-16 15:27:24 +1000

    Update access.php
    chatgpt

--- db/access.php
+++ db/access.php
@@ -2,42 +2,37 @@
 /**
  * Moodec Capability definitions
  *
- * @package     local
- * @subpackage  local_moodec
- * @author   	Thomas Threadgold
+ * @package     local_moodec
+ * @author      Vernon Spain - formerly Thomas Threadgold
  * @copyright   2015 LearningWorks Ltd
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
+defined('MOODLE_INTERNAL') || die();
 
-$capabilities = array(
+$capabilities = [
 
-	'local/moodec:manage' => array(
+    'local/moodec:manage' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
 
-		'riskbitmask' => RISK_SPAM,
-
-		'captype' => 'write',
-		'contextlevel' => CONTEXT_COURSE,
-		'archetypes' => array(
-			'editingteacher' => CAP_ALLOW,
-			'manager' => CAP_ALLOW,
-		),
-	),
-
-	'local/moodec:viewalltransactions' => array(
-
-		'riskbitmask' => RISK_PERSONAL,
-
-		'captype' => 'read',
-		'contextlevel' => CONTEXT_SYSTEM,
-		'archetypes' => array(
-			'guest' => CAP_PREVENT,
-			'user' => CAP_PREVENT,
-			'student' => CAP_PREVENT,
-			'teacher' => CAP_PREVENT,
-			'editingteacher' => CAP_ALLOW,
-			'manager' => CAP_ALLOW,
-		),
-	),
-);
+    'local/moodec:viewalltransactions' => [
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'guest' => CAP_PREVENT,
+            'user' => CAP_PREVENT,
+            'student' => CAP_PREVENT,
+            'teacher' => CAP_PREVENT,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+];
