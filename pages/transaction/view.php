24b6293f945625cf6b57603d34fc8d9c6b90d2de
Vernon Spain <verzog@gmail.com>
2025-07-16 16:00:10 +1000

    Update view.php
    chatgptThe single transaction page has now been updated to follow your requirements and use best practices for Moodle 5.0 and PHP 8.2+. Let me know if you'd like help updating the single_transaction() renderer or adjusting formatting/output.

--- pages/transaction/view.php
+++ pages/transaction/view.php
@@ -2,79 +2,61 @@
 /**
  * Moodec Single Transaction Page
  *
- * @package     local
- * @subpackage  local_moodec
- * @author   	Thomas Threadgold
+ * @package     local_moodec
+ * @author      Thomas Threadgold, OpenAI Updates
  * @copyright   2015 LearningWorks Ltd
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once dirname(__FILE__) . '/../../../../config.php';
-require_once $CFG->dirroot . '/local/moodec/lib.php';
+require_once(__DIR__ . '/../../../../config.php');
+require_once($CFG->dirroot . '/local/moodec/lib.php');
 
-$params = array();
-
-// Get the ID of the user to be displayed
-$transactionID = optional_param('id', 0, PARAM_INT);
-
-if( !!$transactionID ) {
-	$params['id'] = $transactionID;
+$transactionid = optional_param('id', 0, PARAM_INT);
+if (!$transactionid) {
+    print_error('invalidtransactionid', 'local_moodec');
 }
 
 $context = context_system::instance();
-
-// Set PAGE variables
-$PAGE->set_context($context);
-$PAGE->set_url($CFG->wwwroot . '/local/moodec/pages/transaction/view.php', $params);
-
-$PAGE->navbar->add(get_string('transactions_title', 'local_moodec'), new moodle_url($CFG->wwwroot . '/local/moodec/pages/transaction/index.php'));
-$PAGE->navbar->add(get_string('transaction_view_title', 'local_moodec', array('id'=>$transactionID)));
-
-// Force the user to login/create an account to access this page
 require_login();
 
-$transaction = new MoodecTransaction($transactionID);
-
-if ( (int) $USER->id !== $transaction->get_user_id() ) {
-	require_capability('local/moodec:viewalltransactions', $context);
+$transaction = new MoodecTransaction($transactionid);
+if (!$transaction) {
+    print_error('invalidtransaction', 'local_moodec');
 }
 
-// Check if the theme has a moodec pagelayout defined, otherwise use standard
-if (array_key_exists('moodec_transaction_view', $PAGE->theme->layouts)) {
-	$PAGE->set_pagelayout('moodec_transaction_view');
-} else if(array_key_exists('moodec', $PAGE->theme->layouts)) {
-	$PAGE->set_pagelayout('moodec');
+if ($USER->id !== $transaction->get_user_id()) {
+    require_capability('local/moodec:viewalltransactions', $context);
+}
+
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/moodec/pages/transaction/view.php', ['id' => $transactionid]));
+$PAGE->navbar->add(get_string('transactions_title', 'local_moodec'),
+    new moodle_url('/local/moodec/pages/transaction/index.php'));
+$PAGE->navbar->add(get_string('transaction_view_title', 'local_moodec', ['id' => $transactionid]));
+
+if (isset($PAGE->theme->layouts['moodec_transaction_view'])) {
+    $PAGE->set_pagelayout('moodec_transaction_view');
+} elseif (isset($PAGE->theme->layouts['moodec'])) {
+    $PAGE->set_pagelayout('moodec');
 } else {
-	$PAGE->set_pagelayout('standard');
+    $PAGE->set_pagelayout('standard');
 }
 
-// Get the renderer for this page
 $renderer = $PAGE->get_renderer('local_moodec');
 
-//needs to have the product verified before setting page heading & title
-$PAGE->set_title(get_string('transaction_view_title', 'local_moodec', array('id'=>$transactionID)));
-$PAGE->set_heading(get_string('transaction_view_title', 'local_moodec', array('id'=>$transactionID)));
+$title = get_string('transaction_view_title', 'local_moodec', ['id' => $transactionid]);
+$PAGE->set_title($title);
+$PAGE->set_heading($title);
 
 echo $OUTPUT->header();
-
-?>
-
-<h1 class="page__title"><?php echo get_string('transaction_view_title', 'local_moodec', array('id'=>$transactionID)); ?></h1>
-
-<?php 
-
-// Render the transaction list
+echo html_writer::tag('h1', $title, ['class' => 'page__title']);
 echo $renderer->single_transaction($transaction);
 
-if( !!$transaction->get_error() && has_capability('local/moodec:viewalltransactions', $context)) {
-	printf(
-		'<div class="span12 desktop-first-column">
-			<h4>%s</h4>
-			<pre>%s</pre>
-		</div>',
-		get_string('transaction_section_error', 'local_moodec'),
-		$transaction->get_error()
-	);
+if ($transaction->get_error() && has_capability('local/moodec:viewalltransactions', $context)) {
+    echo html_writer::start_div('transaction-error span12 desktop-first-column');
+    echo html_writer::tag('h4', get_string('transaction_section_error', 'local_moodec'));
+    echo html_writer::tag('pre', s($transaction->get_error()));
+    echo html_writer::end_div();
 }
 
 echo $OUTPUT->footer();
