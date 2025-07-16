f056482cc416749cae2ced1b8f53fbbd98696afd
Vernon Spain <verzog@gmail.com>
2025-07-16 15:36:47 +1000

    Update upgrade.php
    chat gpt fix

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,23 +1,55 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-function xmldb_local_moodec_upgrade($oldversion) {
+/**
+ * Upgrade script for local_moodec plugin.
+ *
+ * @package    local_moodec
+ * @category   upgrade
+ * @copyright  2025 Vernon Spain
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Execute Moodec plugin upgrade steps.
+ *
+ * @param int $oldversion
+ * @return bool
+ */
+function xmldb_local_moodec_upgrade(int $oldversion): bool {
     global $DB;
+
     $dbman = $DB->get_manager();
 
+    // Example upgrade step (legacy from original version).
     if ($oldversion < 2015100600) {
-
-        // Define field error to be added to local_moodec_transaction.
         $table = new xmldb_table('local_moodec_transaction');
         $field = new xmldb_field('error', XMLDB_TYPE_TEXT, null, null, null, null, null, 'purchase_date');
 
-        // Conditionally launch add field error.
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
 
-        // Moodec savepoint reached.
         upgrade_plugin_savepoint(true, 2015100600, 'local', 'moodec');
     }
+
+    // Future upgrade steps go below, e.g.:
+    // if ($oldversion < 2025071601) { ... }
 
     return true;
 }
