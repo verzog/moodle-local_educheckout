a666e87541e47a822c7f577af62709d0de828570
Vernon Spain <verzog@gmail.com>
2025-07-16 16:01:31 +1000

    Update cart.php
    The cart.php file has been fully updated for Moodle 5.0 and PHP 8.2. Changes include:
    
    Modern require_once(__DIR__) usage.
    
    Consistent moodle_url() usage.
    
    Improved parameter handling using optional_param().
    
    Structured and simplified switch block for cart actions.
    
    Safer HTML output via html_writer.

--- pages/cart.php
+++ pages/cart.php
@@ -1,100 +1,79 @@
 <?php
 /**
- *Moodec Cart Page
+ * Moodec Cart Page
  *
- * @package     local
- * @subpackage  local_moodec
- * @author   	Thomas Threadgold
+ * @package     local_moodec
+ * @author      Thomas Threadgold, OpenAI Updates
  * @copyright   2015 LearningWorks Ltd
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once dirname(__FILE__) . '/../../../config.php';
-require_once $CFG->dirroot . '/local/moodec/lib.php';
+require_once(__DIR__ . '/../../../config.php');
+require_once($CFG->dirroot . '/local/moodec/lib.php');
 
-$systemcontext = context_system::instance();
+require_login();
+$context = context_system::instance();
 
-$PAGE->set_context($systemcontext);
-$PAGE->set_url($CFG->wwwroot . '/local/moodec/pages/cart.php');
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/moodec/pages/cart.php'));
 
-// Check if the theme has a moodec pagelayout defined, otherwise use standard
-if (array_key_exists('moodec_cart', $PAGE->theme->layouts)) {
-	$PAGE->set_pagelayout('moodec_cart');
-} else if(array_key_exists('moodec', $PAGE->theme->layouts)) {
-	$PAGE->set_pagelayout('moodec');
+if (isset($PAGE->theme->layouts['moodec_cart'])) {
+    $PAGE->set_pagelayout('moodec_cart');
+} elseif (isset($PAGE->theme->layouts['moodec'])) {
+    $PAGE->set_pagelayout('moodec');
 } else {
-	$PAGE->set_pagelayout('standard');
+    $PAGE->set_pagelayout('standard');
 }
 
 $PAGE->set_title(get_string('cart_title', 'local_moodec'));
 $PAGE->set_heading(get_string('cart_title', 'local_moodec'));
-
-// Get the renderer for this page
 $renderer = $PAGE->get_renderer('local_moodec');
 
-//
-// Check for new items and add to cart
-//
-
-// Get the cart in it's current state
 $cart = new MoodecCart();
+$action = optional_param('action', '', PARAM_ALPHA);
+$productid = optional_param('id', 0, PARAM_INT);
+$variation = optional_param('variation', null, PARAM_ALPHANUMEXT);
 
-// If we are adding to the cart, process this first
-if (isset($_POST['action']) && $_POST['action'] === 'addToCart') {
-	// Updates the cart var with the new addition
-	$cart->add($_POST['id']);
-	// redirect back to the course page
-	redirect(new moodle_url($CFG->wwwroot . '/local/moodec/pages/cart.php'));
-}
+switch ($action) {
+    case 'addToCart':
+        if ($productid) {
+            $cart->add($productid);
+        }
+        redirect(new moodle_url('/local/moodec/pages/cart.php'));
+        break;
 
-// If we are adding to the cart, process this first
-if (isset($_POST['action']) && $_POST['action'] === 'addVariationToCart') {
-	// Updates the cart var with the new addition
-	$cart->add($_POST['id'], $_POST['variation']);
-	// redirect back to the course page
-	redirect(new moodle_url($CFG->wwwroot . '/local/moodec/pages/cart.php'));
-}
+    case 'addVariationToCart':
+        if ($productid && $variation !== null) {
+            $cart->add($productid, $variation);
+        }
+        redirect(new moodle_url('/local/moodec/pages/cart.php'));
+        break;
 
-if (isset($_POST['action']) && $_POST['action'] === 'removeFromCart') {
-	// Updates the cart var with the new addition
-	$cart->remove($_POST['id']);
-	// redirect back to the course page
-	redirect(new moodle_url($CFG->wwwroot . '/local/moodec/pages/cart.php'));
-}
+    case 'removeFromCart':
+        if ($productid) {
+            $cart->remove($productid);
+        }
+        redirect(new moodle_url('/local/moodec/pages/cart.php'));
+        break;
 
-if (isset($_POST['action']) && $_POST['action'] === 'emptyCart') {
-	// Updates the cart var with the new addition
-	$cart->clear();
-	// redirect back to the course page
-	redirect(new moodle_url($CFG->wwwroot . '/local/moodec/pages/catalogue.php'));
+    case 'emptyCart':
+        $cart->clear();
+        redirect(new moodle_url('/local/moodec/pages/catalogue.php'));
+        break;
 }
 
 echo $OUTPUT->header();
-
-// Render page title
-printf('<h1 class="page__title">%s</h1>', get_string('cart_title', 'local_moodec'));
-
-// Render the product page content
+echo html_writer::tag('h1', get_string('cart_title', 'local_moodec'), ['class' => 'page__title']);
 echo $renderer->moodec_cart($cart);
 
-
-$relatedOutput = '';
-// Check if there are any related products for each product in the cart
-foreach($cart->get() as $id => $variation) {
-	
-	// Get the product for the given ID
-	$product = local_moodec_get_product($id);
-		
-	// Get the HTML output of the related products renderer
-	$relatedOutput = $renderer->related_products($product);
-
-	// If there is any output, then we are happy and can continue!
-	if( $relatedOutput !== '') {
-		break;
-	}
+// Render related products from first product found that has any
+foreach ($cart->get() as $id => $variation) {
+    $product = local_moodec_get_product($id);
+    $related = $renderer->related_products($product);
+    if (!empty($related)) {
+        echo $related;
+        break;
+    }
 }
-
-echo $relatedOutput;
-
 
 echo $OUTPUT->footer();
