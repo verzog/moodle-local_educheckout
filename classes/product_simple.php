f4a6b648c116f762b8b3a232a1466eeae0cf8339
Vernon Spain <verzog@gmail.com>
2025-07-16 15:06:16 +1000

    Update product_simple.php
    chatgpt

--- classes/product_simple.php
+++ classes/product_simple.php
@@ -1,61 +1,53 @@
-<?php 
+<?php
 /**
  * Moodec Simple Product
  *
  * @package     local
  * @subpackage  local_moodec
- * @author   	Thomas Threadgold
+ * @author      Thomas Threadgold
  * @copyright   2015 LearningWorks Ltd
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Load Moodle config
-require_once dirname(__FILE__) . '/../../../config.php';
-// Load Moodec lib
-require_once dirname(__FILE__) . '/../lib.php';
+namespace local_moodec;
 
-class MoodecProductSimple extends MoodecProduct {
+use moodle_exception;
 
-	/**
-	 * A single product variation
-	 * @var MoodecProductVariation
-	 */
-	protected $_simpleVariation;
+require_once(__DIR__ . '/../../../config.php');
+require_once(__DIR__ . '/../lib.php');
+require_once(__DIR__ . '/product.php');
 
-	/**
-	 * Loads the product from the DB, incuding simple product info
-	 * @param  int 		$id		MoodecProduct id
-	 * @return [type]     		[description]
-	 */
-	public function load($id) {
-		parent::load($id);
+class product_simple extends product {
+    protected float $_price;
 
-		$this->_simpleVariation = new MoodecProductVariation($id);
-	}
+    public function __construct(?int $id = null) {
+        parent::__construct($id);
 
-	/**
-	 * Maps the enrolment duration function to the ProductVariation method
-	 * @param  boolean $format 		If true, then format the duration as a string
-	 * @return string/int       	Duration as a string, or int
-	 */
-	public function get_duration($format = true){
-		return $this->_simpleVariation->get_duration($format);
-	}
+        if (!is_null($id)) {
+            $this->load_simple_data($id);
+        }
+    }
 
-	/**
-	 * Maps the price function to the ProductVariation method
-	 * @return float price
-	 */
-	public function get_price(){
-		return $this->_simpleVariation->get_price();
-	}
+    /**
+     * Load simple product specific data.
+     */
+    protected function load_simple_data(int $id): void {
+        global $DB;
 
-	/**
-	 * Maps the group getter function to the ProductVariation method
-	 * @return int 		groupid
-	 */
-	public function get_group(){
-		return $this->_simpleVariation->get_group();
-	}
+        $record = $DB->get_record('local_moodec_product_simple', ['product_id' => $id], '*', IGNORE_MISSING);
 
+        if ($record) {
+            $this->_price = (float) $record->price;
+        } else {
+            throw new moodle_exception('missingproductpricedata', 'local_moodec', '', $id);
+        }
+    }
+
+    public function get_price(): float {
+        return $this->_price;
+    }
+
+    public function has_price(): bool {
+        return $this->_price > 0;
+    }
 }
