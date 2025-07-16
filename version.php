4b22930ac20cfc2548a4e5e091ab98e49faacf03
Vernon Spain <verzog@gmail.com>
2025-07-16 14:44:51 +1000

    Update version.php
    chatGPT

--- version.php
+++ version.php
@@ -2,21 +2,19 @@
 /**
  * Moodec Version file
  *
- * @package     local
- * @subpackage  local_moodec
- * @author   	Thomas Threadgold
+ * @package     local_moodec
+ * @author      Vernon Spain - Originally Thomas Threadgold
  * @copyright   2015 LearningWorks Ltd
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
+defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_moodec';
-$plugin->version = 2015100600;
-$plugin->release = '2.8 (Build: 2015012900)';
-$plugin->requires = 2014051200;
-//$plugin->requires = 2014111000;
-$plugin->maturity = MATURITY_BETA;
-$plugin->dependencies = array(
-	'enrol_moodec' => 2014111000,
-);
+$plugin->version   = 2025071600; // YYYYMMDDXX format, increment with each update.
+$plugin->release   = '5.0.0 (Build: 2025071600)';
+$plugin->requires  = 2023042400; // Moodle 4.3+ required (adjust to 2024041900 for Moodle 5.0).
+$plugin->maturity  = MATURITY_BETA;
+$plugin->dependencies = [
+    'enrol_moodec' => 2024041900,
+];
