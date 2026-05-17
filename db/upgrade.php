04dc0611f652dbdfc4d72c4abf22e1486c931cbc
Vernon Spain <verzog@gmail.com>
2026-05-17 20:00:12 +1000

    local_moodec: Moodle 5.0+ foundation (version, CI, schema, upgrade)
    - version.php: Moodle 5.0+ baseline, standard header, depends on the rebuilt
      enrol_moodec (2026051700); retains 2015 LearningWorks attribution.\n- .github/workflows/moodle-ci.yml: same proven config as enrol_moodec\n  (pgsql+mariadb, concurrency, AU TZ) plus a checkout of enrol_moodec as an\n  extra plugin so the dependency resolves during install.\n- db/install.xml: retains the legacy product/variation/transaction tables\n  (Decision 3 - not migrated) and adds the four new cart/order tables.\n- db/upgrade.php: creates the four new tables on upgrade via\n  install_one_table_from_xmldb_file with table_exists guards.\n\nCI will be red on phpcs until the legacy code is replaced (infra-first, as\nwith enrol_moodec).

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,22 +1,55 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
+/**
+ * Upgrade steps for the Moodec storefront plugin.
+ *
+ * @package    local_moodec
+ * @copyright  2026 LearningWorks Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Upgrade the Moodec storefront plugin.
+ *
+ * @param int $oldversion the version we are upgrading from
+ * @return bool
+ */
 function xmldb_local_moodec_upgrade($oldversion) {
-    global $DB;
+    global $CFG, $DB;
+
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2015100600) {
-
-        // Define field error to be added to local_moodec_transaction.
-        $table = new xmldb_table('local_moodec_transaction');
-        $field = new xmldb_field('error', XMLDB_TYPE_TEXT, null, null, null, null, null, 'purchase_date');
-
-        // Conditionally launch add field error.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
+    if ($oldversion < 2026051700) {
+        $newtables = [
+            'local_moodec_cart',
+            'local_moodec_cart_item',
+            'local_moodec_order',
+            'local_moodec_order_item',
+        ];
+        foreach ($newtables as $tablename) {
+            $table = new xmldb_table($tablename);
+            if (!$dbman->table_exists($table)) {
+                $dbman->install_one_table_from_xmldb_file(
+                    $CFG->dirroot . '/local/moodec/db/install.xml',
+                    $tablename
+                );
+            }
         }
-
-        // Moodec savepoint reached.
-        upgrade_plugin_savepoint(true, 2015100600, 'local', 'moodec');
+        upgrade_plugin_savepoint(true, 2026051700, 'local', 'moodec');
     }
 
     return true;
